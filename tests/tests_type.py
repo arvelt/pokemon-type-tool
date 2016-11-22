92f4120d2858558480b07ed81a1efaeface46212
Keisuke Ota <arvelt.s@gmail.com>
2016-11-23 03:21:54 +0900

    secound commit

--- tests/tests_type.py
+++ tests/tests_type.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-import pytest
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
-
 from src.main import Type
 
 class TestType(object):
