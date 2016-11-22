7392b2cc0231d680e097911d2de97e82e21ffbfa
Keisuke Ota <arvelt.s@gmail.com>
2016-11-23 04:40:21 +0900

    check_typeがうごくっぽい

--- src/main.py
+++ src/main.py
@@ -5,31 +5,33 @@ u"""
 こうかはいまひとつのようだ…：It's not very effective…
 (ポケモン)には効果がないみたいだ…：It doesn't affect (ポケモン)!
 """
-NORMAL_EFFECT = 0
-SUPER_EFFECT = 1
-FEW_EFFECT = 2
-NOTHING_EFFECT = 9
-
-NOMAL = 0
-FIRE = 1
-WATER = 2
-GRASS = 3
-ELECTRIC = 4
-ICE = 5
-FIGHTING = 6
-POISON = 7
-GROUND = 8
-FLYING = 9
-PSYCHIC = 10
-BUG = 11
-ROCK = 12
-GHOST = 13
-DRAGON = 14
-DARK = 15
-STEEL = 16
-FAIRY = 17
 
 class Type(object):
+    NORMAL_EFFECT = 0
+    SUPER_EFFECT = 1
+    FEW_EFFECT = 2
+    DOUBLE_SUPER_EFFECT = 4
+    NOTHING_EFFECT = 9
+
+    NORMAL = 0
+    FIRE = 1
+    WATER = 2
+    ELECTRIC = 3
+    GRASS = 4
+    ICE = 5
+    FIGHTING = 6
+    POISON = 7
+    GROUND = 8
+    FLYING = 9
+    PSYCHIC = 10
+    BUG = 11
+    ROCK = 12
+    GHOST = 13
+    DRAGON = 14
+    DARK = 15
+    STEEL = 16
+    FAIRY = 17
+
     type_table = [
         [0,0,0,0,0,0,0,0,0,0,0,0,2,9,0,0,2,0],
         [0,2,2,0,1,1,0,0,0,0,0,1,2,0,2,0,1,0],
@@ -62,5 +64,45 @@ class Type(object):
     return True or False or None
     """
     @classmethod
-    def check_effect(cls, type, target_type1, target_type2):
-        return 0
+    def check_effect(cls, _type, *args):
+        length = len(args)
+        if length == 1:
+            target_type1 = args[0]
+            return cls.type_table[_type][target_type1]
+        elif length == 2:
+            target_type1 = args[0]
+            target_type2 = args[1]
+            type1 = cls.type_table[_type][target_type1]
+            type2 = cls.type_table[_type][target_type2]
+
+            def is_nothing(_type):
+                return _type == cls.NOTHING_EFFECT
+
+            def is_super(_type):
+                return _type == cls.SUPER_EFFECT
+
+            def is_few(_type):
+                return _type == cls.FEW_EFFECT
+
+            if is_nothing(type1) or is_nothing(type2):
+                # こうかはない
+                return cls.NOTHING_EFFECT
+            elif is_super(type1) and is_super(type2):
+                # 4倍
+                return cls.DOUBLE_SUPER_EFFECT
+            elif is_super(type1) and is_few(type2):
+                # 打ち消し
+                return cls.NORMAL
+            elif is_super(type2) and is_few(type1):
+                # 打ち消し
+                return cls.NORMAL
+            elif is_super(type1) or is_super(type2):
+                # こうかはばつぐん
+                return cls.SUPER_EFFECT
+            elif is_few(type1) or is_few(type2):
+                # こうかはいまひとつ
+                return cls.SUPER_EFFECT
+            else:
+                return cls.NORMAL_EFFECT
+        else:
+            raise TypeError('check_effect() takes less than 4 arguments')
--- tests/tests_type.py
+++ tests/tests_type.py
@@ -2,8 +2,35 @@
 from src.main import Type
 
 class TestType(object):
-    def test_get_super_effective(self):
-        assert Type.get_super_effective(0, 1) == 0
+    def test_table_super_effective(self):
+        assert Type.type_table[Type.WATER][Type.FIRE] == 1
+        assert Type.type_table[Type.FIGHTING][Type.ICE] == 1
+        assert Type.type_table[Type.GROUND][Type.POISON] == 1
+
+    def test_table_few_effective(self):
+        assert Type.type_table[Type.FIRE][Type.WATER] == 2
+        assert Type.type_table[Type.GRASS][Type.FIRE] == 2
+
+    def test_table_nothing_effective(self):
+        assert Type.type_table[Type.NORMAL][Type.GHOST] == 9
+        assert Type.type_table[Type.ELECTRIC][Type.GROUND] == 9
+        assert Type.type_table[Type.FIGHTING][Type.GHOST] == 9
+        assert Type.type_table[Type.POISON][Type.STEEL] == 9
+        assert Type.type_table[Type.GROUND][Type.FLYING] == 9
+        assert Type.type_table[Type.GHOST][Type.NORMAL] == 9
+        assert Type.type_table[Type.DRAGON][Type.FAIRY] == 9
+
+    def test_table_normal_effective(self):
+        assert Type.type_table[Type.GROUND][Type.GHOST] == Type.NORMAL_EFFECT
+
+    def test_check_effect1(self):
+        assert Type.check_effect(Type.ELECTRIC, Type.WATER, Type.FLYING) == Type.DOUBLE_SUPER_EFFECT  # ギャラドスにかみなり
+        assert Type.check_effect(Type.ICE, Type.DRAGON, Type.FLYING) == Type.DOUBLE_SUPER_EFFECT  # カイリューにこおり
+        assert Type.check_effect(Type.GROUND, Type.GHOST, Type.POISON) == Type.SUPER_EFFECT  # ゲンガーにじめん
+        assert Type.check_effect(Type.POISON, Type.GRASS, Type.POISON) == Type.NORMAL_EFFECT  # フシギバナにどく
+        assert Type.check_effect(Type.ROCK, Type.STEEL, Type.FLYING) == Type.NORMAL_EFFECT  # エアームドにいわ
+        assert Type.check_effect(Type.ELECTRIC, Type.ROCK, Type.GROUND) == Type.NOTHING_EFFECT  # サイドンにじめん
+        assert Type.check_effect(Type.DRAGON, Type.WATER, Type.FAIRY) == Type.NOTHING_EFFECT  # マリルリにドラゴン
 
     def test_get_super_effective(self):
-        assert Type.get_super_effective(0, 1) == 0
+        pass
