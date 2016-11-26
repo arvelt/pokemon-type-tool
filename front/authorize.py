22789fda91c4890d798bf8b2fd594903a16c321d
Keisuke Ota <arvelt.s@gmail.com>
2016-11-26 19:31:20 +0900

    クレデンシャルを使い分ける

--- front/authorize.py
+++ front/authorize.py
@@ -1,17 +1,27 @@
-
-from oauth2client.service_account import ServiceAccountCredentials
+# -*- coding: utf-8 -*-
 from httplib2 import Http
 from apiclient import discovery
 
 from google.appengine.ext import ndb
 
+
+SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+
+def get_credential():
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
+        from oauth2client.contrib.appengine import AppAssertionCredentials
+        return credentials = AppAssertionCredentials(SCOPE)
+    else:
+        from oauth2client.service_account import ServiceAccountCredentials
+        key = ndb.Key('SericeAccountToken', 'pkmn-tool-service-account')
+        entity = key.get()
+        if entity is None:
+            return None
+        return ServiceAccountCredentials.from_json_keyfile_dict(entity.credential, SCOPE)
+
+
 def build_client():
-    key = ndb.Key('SericeAccountToken', 'pkmn-tool-service-account')
-    entity = key.get()
-    if entity is None:
-        return None
-    scope = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(entity.credential, scope)
     http_auth = credentials.authorize(Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
     return discovery.build('sheets', 'v4', http=http_auth, discoveryServiceUrl=discoveryUrl)
